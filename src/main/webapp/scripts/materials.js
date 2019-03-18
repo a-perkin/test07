64c5cf09f2e8d346d1fcacd9763a38857ceee905
a-perkin <aleksandr.perkin90gmail.com>
2019-03-19 00:40:16 +0200

    js remove row   edit row (in progress)

--- src/main/webapp/scripts/materials.js
+++ src/main/webapp/scripts/materials.js
@@ -1,12 +1,13 @@
 //показать / скрыть материалы
 let materials = document.getElementById("materials");
 let menuFireplace = document.getElementById("menuFireplace");
-let listMaterials = document.getElementById("listMaterials");
 let menuMaterials = document.getElementById("menuMaterials");
+let tableMaterials = document.getElementById('tableMaterials');
 let aoMaterials = null;
 let oMaterial = {};
-//строка в таблице с материалами
-let sMaterials = "";
+//Содержание таблицы материалов в хтмл виде
+sMaterials = "";
+
 let obj;
 
 let visible = false;
@@ -21,35 +22,81 @@ function showMaterials() {
 }
 
 //Добавить материал в таблицу
-function addMaterialToTable(arr) {
-
-    for (let i = 0; i < arr.length; i++) {
-        obj = arr[i];
-        sMaterials = sMaterials + "<tr id=\"" + obj.id +"\"><td>" + obj.name + "</td><td>" + obj.price + "</td><td>" + obj.thickness + "</td></tr>";
-
-    }
-    let tableMaterials = document.getElementById('tableMaterials');
-    tableMaterials.insertAdjacentHTML('beforeend', sMaterials);
-}
-
-//получить список всех материалов
+// function addMaterialToTable(arr) {
+//
+//     for (let i = 0; i < arr.length; i++) {
+//         obj = arr[i];
+//         sMaterials = sMaterials + "<tr id=\"" + obj.id +"\"><td>" + obj.name + "</td><td>" + obj.price + "</td><td>" + obj.thickness + "</td></tr>";
+//
+//     }
+//     tableMaterials.insertAdjacentHTML('beforeend', sMaterials);
+// }
+//
+//получить список всех материалов + засетить в таблицу
 menuMaterials.onclick = function () {
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
-        if (xhr.status == 200 && xhr.readyState === 4 && aoMaterials === null){
+        if (xhr.status == 200 && xhr.readyState === 4 && aoMaterials === null){ //последняя проверка временно, что бы при каждом клике не дергало рест
             aoMaterials = JSON.parse(xhr.response);
             console.log(aoMaterials);
-            // for (let i = 0; i < aoMaterials.length; i++) {
-            //     if (listMaterials.childNodes.length <= aoMaterials.length){
-            //         addMaterialToList(aoMaterials[i].name, aoMaterials[i].thickness, aoMaterials[i].price);
-            //     }
-            // }
-            addMaterialToTable(aoMaterials);
+            for (let i = 0; i < aoMaterials.length; i++) {
+                oMaterial = aoMaterials[i];
+                addSampleRow(oMaterial);
+            }
         }
     };
     xhr.open('GET', '/rest/materials', true);
     xhr.send();
 };
+
+function addSampleRow(oMaterial) {
+    let table = document.getElementById('tableMaterials');
+    let row = table.insertRow(table.rows.length);
+    let cellName = row.insertCell(0);
+    let cellPrice = row.insertCell(1);
+    let cellThickness = row.insertCell(2);
+    let cellEdit = row.insertCell(3);
+    let cellRemove = row.insertCell(4);
+
+    row.id = 'material_' + oMaterial.id;
+    cellName.innerHTML = oMaterial.name;
+    cellPrice.innerHTML = oMaterial.price;
+    cellThickness.innerHTML = oMaterial.thickness;
+
+    let btnEdit = document.createElement('input');
+    btnEdit.setAttribute("type", "button");
+    btnEdit.setAttribute("value", "Edit");
+    btnEdit.setAttribute("name", "Edit");
+    btnEdit.setAttribute("onclick", 'editRowMaterial(this.parentNode.parentNode.id)');
+    cellEdit.appendChild(btnEdit);
+
+    let btnRemove = document.createElement('input');
+    btnRemove.setAttribute("type", "button");
+    btnRemove.setAttribute("value", "Remove");
+    btnRemove.setAttribute("name", "Remove");
+    btnRemove.setAttribute("onclick", 'deleteRowMaterial(this.parentNode.parentNode.id)');
+    cellRemove.appendChild(btnRemove);
+}
+
+function deleteRowMaterial(rowId) {
+    let nID = rowId.split('_')[1];
+    //console.log(nID);
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", '/rest/materials/delete?id=' + nID, true);
+
+    let row = document.getElementById(rowId);
+    let table = row.parentNode;
+    while (table && table.tagName != 'TABLE'){
+        table = table.parentNode;
+        if(!table){
+            return;
+        }
+        if (confirm("Удалить материал " + row.childNodes[0].innerHTML + "?")){
+            table.deleteRow(row.rowIndex);
+            xhr.send();
+        }
+    }
+}
 
 //Создать новый материал (новую запись в базе)
 document.forms.material.onsubmit = function() {
@@ -63,7 +110,7 @@ document.forms.material.onsubmit = function() {
     return false;
 };
 
-//Пост запрос
+//Пост запрос на получение всех материалов
 function addMaterial(obj){
     console.log("obj = " + JSON.stringify(obj));
     let xhr = new XMLHttpRequest();
@@ -74,7 +121,43 @@ function addMaterial(obj){
     xhr.onload = function () {
         if (this.status == 200) {
             console.log('response', this.response); // JSON response
+            alert(JSON.stringify(this.response));
+            addSampleRow(this.response);
+            document.forms.material.reset();
         }
     };
 }
+
+//Редактирование строки таблицы материалов
+function editRowMaterial(rowId) {
+    //let nID = rowId.split('_')[1];
+    //console.log(nID);
+    let row = document.getElementById(rowId);
+    row.setAttribute("currentLine", true);
+    row.style.background = 'yellow';
+
+    let arrTD = row.childNodes;
+    for(let i = 0; i < arrTD.length - 2; i++){
+        let textArea = document.createElement('input');
+        textArea.value = arrTD[i].innerHTML;
+        arrTD[i].innerHTML = '';
+        arrTD[i].appendChild(textArea);
+    }
+    saveRowMaterial(arrTD);
+}
+
+
+function saveRowMaterial(arrTD) {
+    let target = event.target;
+    if (target.nodeName == 'button'){
+        if(confirm("Сохранить?")){
+            for(let i = 0; i < arrTD.length - 2; i++) {
+                arrTD[i].innerHTML = arrTD[i].firstChild.value;
+            }
+        }
+    }
+    console.log(target)
+
+}
+
 
